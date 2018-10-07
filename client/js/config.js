02c60d8976e349441c5a9c52b92aebfda0947c1e
JTrancender <jie-email@jie-trancender.org>
2018-10-07 23:52:58 +0800

    临时调整

---
+++ client/js/config.js
@@ -0,0 +1,11 @@
+// export let serverDomain = 'local.jie-trancender.org'
+// export let salt = '582865471'
+
+let serverDomain = 'local.jie-trancender.org'
+let salt = '582865471'
+
+function testExport() {
+	console.log('testExport')
+}
+
+export {serverDomain, salt, testExport}
